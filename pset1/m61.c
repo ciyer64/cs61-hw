36d2f7e3f494215da62da6d1920a4ca70c0472ee
ciyer64 <curreniyer@college.harvard.edu>
2016-09-12 21:46:55 -0400

    tests 1-11 working

--- pset1/m61.c
+++ pset1/m61.c
@@ -25,40 +25,32 @@ typedef struct meta61 {
 
 void* m61_malloc(size_t sz, const char* file, int line) {
     (void) file, (void) line;   // avoid uninitialized variable warnings
-    
     size_t max_size = (size_t) -1;
     max_size -= sizeof(meta61);
-
     if (sz > max_size) {
         stat61.nfail++;
         stat61.fail_size += sz;
         return NULL;
     }
-
     if (sz==0) {
         return NULL;
     }
-
     //char* ptr = base_malloc(sz);
     meta61 *ptr = malloc(sz + sizeof(meta61));
-
     if (!ptr) {
         stat61.nfail++;
         stat61.fail_size += sz;
         return NULL;
     }
-
     else {
         stat61.nactive++;
         stat61.active_size += sz;
         stat61.ntotal++;
         stat61.total_size += sz;
-
         meta61 meta;
         meta.size = sz;
         memcpy(ptr,&meta, sizeof(meta61));
         meta.pntr = (char*) (ptr + sizeof(meta61));
-
         if (stat61.heap_min == NULL && stat61.heap_max == NULL) {
             stat61.heap_min = meta.pntr;
             stat61.heap_max = meta.pntr + sz;
@@ -69,10 +61,9 @@ void* m61_malloc(size_t sz, const char* file, int line) {
         else if (stat61.heap_max < meta.pntr + sz) {
             stat61.heap_max = meta.pntr + sz;
         }
-
     }
     // Your code here.
-    return ptr + sizeof(meta61);
+    return (void*) (ptr + sizeof(meta61));
 }
 
 
@@ -106,13 +97,20 @@ void m61_free(void *ptr, const char *file, int line) {
 ///    location `file`:`line`.
 
 void* m61_realloc(void* ptr, size_t sz, const char* file, int line) {
-    void* new_ptr = NULL;
+    meta61* new_ptr = NULL;
     if (sz)
         new_ptr = m61_malloc(sz, file, line);
     if (ptr && new_ptr) {
         // Copy the data from `ptr` into `new_ptr`.
         // To do that, we must figure out the size of allocation `ptr`.
         // Your code here (to fix test012).
+        meta61 *itismeta = ptr;
+        meta61 *meta = itismeta - sizeof(meta61);
+        meta61 new_meta;
+        new_meta.size = meta->size;
+        new_ptr -= sizeof(meta61);
+        memcpy(new_ptr,&new_meta,sizeof(meta61));
+        new_ptr += sizeof(meta61);
     }
     m61_free(ptr, file, line);
     return new_ptr;
